be6521acbbb3393b21c0505e835848c6b7e957e4
saml99 <sam.lewis@enett.com>
2021-05-29 22:58:27 +1000

    Stacked barchart

--- Scripts/script.js
+++ Scripts/script.js
@@ -870,7 +870,7 @@ function init() {
         });
     };
 
-    d3.csv("BarChartData.csv", function(d, i, columns) {
+    /*d3.csv("BarChartData.csv", function(d, i, columns) {
             for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
             d.total = t;
             return d;
@@ -976,7 +976,7 @@ function init() {
             .attr("x",0 - (h / 2))
             .attr("dy", "1em")
             .text("Total Waste");
-    };
+    };*/
 }
 
 window.onload = init;
