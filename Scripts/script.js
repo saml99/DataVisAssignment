dd60a2dc6adeb471c7608f6017cba9d8f1e76ce7
saml99 <sam.lewis@enett.com>
2021-05-31 21:23:36 +1000

    Mouse tooltip

--- Scripts/script.js
+++ Scripts/script.js
@@ -1155,7 +1155,7 @@ function init() {
             .style("display", "none");
               
         tooltip.append("rect")
-            .attr("width", 30)
+            .attr("width", 60)
             .attr("height", 20)
             .attr("fill", "white")
             .style("opacity", 0.5);
